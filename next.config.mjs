cf4dbf6b1f41ab364232962690aec2a2536df744
Goeasy777 <harsh04a@gmail.com>
2025-01-23 09:12:10 +0530

    Update next.config.mjs "my"

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:3000/:path",
+        destination: "http://localhost:3000/:path*",
 
         // destination: "https://api.unfulo.in/:path*",
 
