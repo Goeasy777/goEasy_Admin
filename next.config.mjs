9ab510149023b76ab688f9c331e7d297e893f92a
MehulSoni171999 <mehulsoni078@gmail.com>
2025-01-27 16:19:49 +0530

    Save local changes before pulling

--- next.config.mjs
+++ next.config.mjs
@@ -2,19 +2,16 @@
 let nextConfig = {};
 
 nextConfig = {
-
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:3000/:path",
+        destination: "https://43.204.97.44/:path*",
 
         // destination: "https://api.unfulo.in/:path*",
-
-      }
+      },
     ];
   },
- 
 };
 
 export default nextConfig;
--- store/Actions/adminActions.js
+++ store/Actions/adminActions.js
@@ -9,11 +9,11 @@ export const iserror = (error) => {
 
 export const adminsignin = (adminData) => async (dispatch) => {
   try {
-    const response = await axios.post("login", adminData,
+    const response = await axios.post("users/login", adminData,
       {
         headers: { "Content-Type": "application/json" },
         withCredentials: true,
-      });
+      }); 
     console.log(response.data);
 
     dispatch({
@@ -33,7 +33,7 @@ export const adminsignin = (adminData) => async (dispatch) => {
 
 export const adminsignup = (adminData) => async (dispatch) => {
   try {
-    const response = await axios.post("/signup", adminData, {
+    const response = await axios.post("users/signup", adminData, {
       headers: { "Content-Type": "application/json" },
       withCredentials: true,
     });
